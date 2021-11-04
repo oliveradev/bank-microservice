f677d7e5e92537859fdc0fe8f0b8cc50b8bec246
Luis Olivera <92808868+oliveradev@users.noreply.github.com>
2021-11-04 12:55:32 -0500

    Error Gateway - Eureka

--- gateway-service/src/main/java/com/everis/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/everis/gatewayservice/GatewayServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
