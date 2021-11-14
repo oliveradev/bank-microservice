943f9b78af138358e338f995f2ab1bc43f7a3d82
ecrisostomop <92813936+ecrisostomop@users.noreply.github.com>
2021-11-13 20:50:05 -0500

    Update CustomerCommand.java

--- yanki-service/src/main/java/com/bootcamp/yankiservice/documents/dto/CustomerCommand.java
+++ yanki-service/src/main/java/com/bootcamp/yankiservice/documents/dto/CustomerCommand.java
@@ -1,5 +1,22 @@
 package com.bootcamp.yankiservice.documents.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerCommand{
+
+    private String name;
+
+    private String identityNumber;
+
+    private String identityType;
+
+    private String phoneNumber;
 
 }
